973af415aa25f73dbfa8a18f7c7e7288c94138e5
KimGo1 <Kimberlygoods36@gmail.com>
2023-11-21 20:13:08 -0600

    Initial commit

---
+++ app/src/main/java/com/ualr/final_project_fall_2023_suicide_squad/UserLoginActivity.java
@@ -0,0 +1,46 @@
+package com.ualr.final_project_fall_2023_suicide_squad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class UserLoginActivity extends AppCompatActivity {
+
+    private EditText editTextUsername, editTextPassword;
+    private Button buttonLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_login_activity);
+
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String username = editTextUsername.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+                if (isValidLogin(username, password)) {
+                    Toast.makeText(UserLoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    Toast.makeText(UserLoginActivity.this, "Invalid username or password", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+    private boolean isValidLogin(String username, String password) {
+        return username.equals("your_username") && password.equals("your_password");
+
+    }
+}
