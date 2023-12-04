41ea61fdb97cc1c30b597c4ce3908927c481e665
Lezly Luna <lluna@ualr.edu>
2023-12-03 18:01:20 -0600

    files and javas added
    Added some xml files to be able to view as grid or list for the dashboard. Added the java file for the dashboard as well to switch between the list or grid views. Made a dummy folderAdapter file while we get the real one done

--- app/src/main/java/com/ualr/final_project_fall_2023_suicide_squad/DashboardOptionsActivity.java
+++ app/src/main/java/com/ualr/final_project_fall_2023_suicide_squad/DashboardOptionsActivity.java
@@ -1,42 +1,72 @@
 package com.ualr.final_project_fall_2023_suicide_squad;
-
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
-import android.content.Intent;
-import androidx.appcompat.widget.Toolbar;
-
+import android.widget.CompoundButton;
+import com.google.android.material.switchmaterial.SwitchMaterial;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatButton;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.content.Intent;
 
 
 public class DashboardOptionsActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private FloatingActionButton fabAddFolder;
+    private SwitchMaterial btnSwitchView;
+
+    private boolean isGridView = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard_options_activity);
 
+        recyclerView = findViewById(R.id.recyclerView);
+        fabAddFolder = findViewById(R.id.fabAddFolder);
+        btnSwitchView = findViewById(R.id.btnSwitchView);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Dashboard Options");
+        // here we will set up RecyclerView with a default layout manager (that being list view)
+        setListLayoutManager();
 
-        //Enable the back button
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        // here we are setting up the click listener for the Switch button
+        btnSwitchView.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (isChecked) {
+                    setGridLayoutManager();
+                } else {
+                    setListLayoutManager();
+                }
+            }
+        });
 
+        // here we are setting up the click listener for the FAB
+        fabAddFolder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Open the NewFolderActivity for adding a new folder
+                startActivity(new Intent(DashboardOptionsActivity.this, NewFolderActivity.class));
+            }
+        });
     }
 
+    List<Folder> folderList = getFolderList();
+    FolderAdapter folderAdapter = new FolderAdapter(folderList);
+        recyclerView.setAdapter(folderAdapter);
 
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            finish();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    private void setListLayoutManager() {
+        isGridView = false;
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        // may need to update the adapter here
+    }
+
+    private void setGridLayoutManager() {
+        isGridView = true;
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        // may need to update the adapter here
     }
 }
